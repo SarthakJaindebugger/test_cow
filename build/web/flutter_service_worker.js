90b82ffa94d58cff255cdefc1453847b871d4e6a
SarthakJaindebugger <shubh2002jain@gmail.com>
2023-07-24 10:19:15 +0530

    updated

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "80b8084792063c2ae8c4220a3dd782dc",
-"index.html": "be52e0ee63400a2d7f481817976fa8df",
-"/": "be52e0ee63400a2d7f481817976fa8df",
+"index.html": "c9377b883a962d078bddeb5d01c3b346",
+"/": "c9377b883a962d078bddeb5d01c3b346",
 "main.dart.js": "9997fcefbb784b98f1d2750876fec88a",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
